5115ad45d525d767f0a76511263ed1c4212eebf1
Jonhyt <jpinto0017@gmail.com>
2021-05-17 21:55:10 +0100

    Web version 0.0.1

---
+++ precache-manifest.7f45e75514caba575dfc331185877b61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c227794e0ce9f2d628b4677193a3981",
+    "url": "/project-psychopompt-elts/index.html"
+  },
+  {
+    "revision": "6fb92d6031a93c4c68b4",
+    "url": "/project-psychopompt-elts/static/css/main.7e935c3f.chunk.css"
+  },
+  {
+    "revision": "6e0d4b2534e63d599028",
+    "url": "/project-psychopompt-elts/static/js/2.18527aa6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/project-psychopompt-elts/static/js/2.18527aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fb92d6031a93c4c68b4",
+    "url": "/project-psychopompt-elts/static/js/main.5c1d5df0.chunk.js"
+  },
+  {
+    "revision": "c7df023d2b5a180d483c",
+    "url": "/project-psychopompt-elts/static/js/runtime-main.974ad84b.js"
+  }
+]);
